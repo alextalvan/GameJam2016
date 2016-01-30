66055fefd5e2f099b5af47e506b4c3b49067bca8
Ohmu93 <dekan51@gmail.com>
2016-01-30 11:20:26 +0100

    basic spawning and ai

---
+++ Assets/Scripts/AI/AIScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIScript : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed = 1f;
+    [SerializeField]
+    private float attackDamage = 1f;
+    [SerializeField]
+    private float attackCoolDown = 1f;
+    private float attackTimer;
+
+    private Transform monks;
+    private Transform targetMonk;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        monks = GameObject.Find("Monks").transform;
+        GetClosestMonk();
+        ResetAttackTimer();
+    }
+
+    void GetClosestMonk()
+    {
+        float closestDist = Mathf.Infinity;
+
+        foreach (Transform monk in monks)
+        {
+            float dist = Vector3.Distance(monk.position, transform.position);
+            if (dist < closestDist)
+            {
+                targetMonk = monk;
+                closestDist = dist;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        UpdateAttackTimer();
+        Chase();
+    }
+
+    void Chase()
+    {
+        Vector2 vel = (targetMonk.position - transform.position).normalized * movementSpeed;
+        rb2d.velocity = vel;
+    }
+
+    void UpdateAttackTimer()
+    {
+        if (attackTimer > 0f)
+            attackTimer -= Time.deltaTime;
+    }
+
+    void ResetAttackTimer()
+    {
+        attackTimer = attackCoolDown;
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Monk" && attackTimer <= 0f)
+        {
+            Debug.Log(gameObject + "attacks");
+            ResetAttackTimer();
+        }
+    }
+}
---
+++ Assets/Scripts/AI/SpawnManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+    [SerializeField]
+    private float spawnCoolDown = 1f;
+    private float spawnTimer;
+    [SerializeField]
+    private GameObject aiPrefab;
+    private Transform AIs;
+
+	// Use this for initialization
+	void Start () {
+        ResetTimer();
+        AIs = GameObject.Find("AIs").transform;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        UpdateTimer();
+        SpawnAI();
+    }
+
+    Vector3 GetRandomSpawnPoint()
+    {
+        int rndIndex = Random.Range(0, 4);
+        Transform rndSpawn = transform.GetChild(rndIndex);
+
+        Vector3 rndSpawnPoint = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        rndSpawnPoint = rndSpawn.TransformPoint(rndSpawnPoint * 0.5f);
+        return rndSpawnPoint;
+    }
+
+    void SpawnAI()
+    {
+        if (spawnTimer <= 0f)
+        {
+            Vector3 rndPos = GetRandomSpawnPoint();
+            GameObject newAI = Instantiate(aiPrefab, rndPos, Quaternion.identity) as GameObject;
+            newAI.transform.parent = AIs;
+            ResetTimer();
+        }
+    }
+
+    void UpdateTimer()
+    {
+        if (spawnTimer > 0f)
+            spawnTimer -= Time.deltaTime;
+    }
+
+    void ResetTimer()
+    {
+        spawnTimer = spawnCoolDown;
+    }
+}
